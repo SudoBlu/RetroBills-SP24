1eaf5ce93cf319a3af013be0b4a998034bda4afd
C00276054 <c00276054@louisiana.edu>
2024-03-09 16:30:52 -0600

    Contact and About Update
    Added content to About and Contact pages
    Updated home page

--- retrobills.client/src/app/contact/contact.component.ts
+++ retrobills.client/src/app/contact/contact.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['../home/home.component.css', './contact.component.css']
 })
 export class ContactComponent {
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
+  /**
+   * Navigates to the login page after clicking the Log In Button
+   */
+  OnLoginClick(){
+    this.router.navigate(['login'])
+  }
+
+  /**
+   * Navigates to the signup page after
+   * clicking the Sign Up button
+   */
+  OnSignUpClick(){
+    this.router.navigate(['signup'])
+  }
+
+  OnAboutClicked(){
+    this.router.navigate(['home/about'])
+  }
+
+  OnContactClicked(){
+    this.router.navigate(['home/contact'])
+  }
+
+  OnHomeClicked(){
+    this.router.navigate([''])
+  }
 }
