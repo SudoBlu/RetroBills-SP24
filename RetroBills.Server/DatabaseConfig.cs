ce45ced78b4380c751329471d79608a8ede161fe
paul <lordbabypanda@gmail.com>
2024-03-18 18:06:43 -0500

    testing out ef core

--- RetroBills.Server/DatabaseConfig.cs
+++ RetroBills.Server/DatabaseConfig.cs
@@ -2,6 +2,8 @@ namespace RetroBills.Server;
 
 public static class DatabaseConfig
 {
+    //public const string ConnectionString =
+    //"Server=retro-bills.database.windows.net;Database=RetroBills;User Id=retro-billsadmin;Password=CMPS490!;";
     public const string ConnectionString =
-        "Server=retro-bills.database.windows.net;Database=RetroBills;User Id=retro-billsadmin;Password=CMPS490!;";
+        "Server=(localdb)\\MSSQLLocalDB;Database=RetroBillsLocalDB;Trusted_Connection=True";
 }
