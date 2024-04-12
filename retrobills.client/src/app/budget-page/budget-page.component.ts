603bdd537429550825efe99b6e8a845cf8cf3a18
C00276054 <c00276054@louisiana.edu>
2024-04-12 11:17:49 -0500

    Updated handling

--- retrobills.client/src/app/budget-page/budget-page.component.ts
+++ retrobills.client/src/app/budget-page/budget-page.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
 import { Account } from '../account';
 import { AccountService } from '../services/account.service';
+import { BudgetsService } from '../services/budgets.service';
+import { Budget } from '../interfaces/budget';
 
 @Component({
   selector: 'app-budget-page',
@@ -10,9 +12,10 @@ import { AccountService } from '../services/account.service';
   styleUrl: './budget-page.component.css'
 })
 export class BudgetPageComponent implements OnInit{
-  constructor(private router: Router, private route: ActivatedRoute, private authService: AuthService, private accountService: AccountService) {}
+  constructor(private router: Router, private route: ActivatedRoute, private authService: AuthService, private accountService: AccountService, private budgetService: BudgetsService) {}
   userId = this.route.snapshot.params['userId']
   accountId = this.route.snapshot.params['accountId']
+  public budget: Budget | undefined;
   public account: Account = {
     accountId: 0,
     accountType: '',
@@ -22,6 +25,7 @@ export class BudgetPageComponent implements OnInit{
   
   async ngOnInit(): Promise<void> {
     this.account = await this.getAccount(this.accountId)
+    this.budget = await this.getBudget(this.accountId);
   }
   OnDashClick(){
     console.log(this.userId)
@@ -37,12 +41,30 @@ export class BudgetPageComponent implements OnInit{
     this.router.navigate(['budget/create', this.userId])
   }
 
+  OnReportsClicked(){
+    this.router.navigate(['budget/report', this.budget])
+  }
+
   getAccount(accountId: number){
     return new Promise<Account>((resolve, reject) => {
       const subscription = this.accountService.getAccountById(accountId).subscribe(response => {
         resolve(response);
+        subscription.unsubscribe();
       }, error => {
         reject(error);
+        subscription.unsubscribe();
+      })
+    })
+  }
+
+  getBudget(accountId: number){
+    return new Promise<Budget>((resolve, reject) => {
+      const subscription = this.budgetService.getBudget(accountId).subscribe(response => {
+        resolve(response);
+        subscription.unsubscribe();
+      }, error => {
+        reject(error);
+        subscription.unsubscribe();
       })
     })
   }
